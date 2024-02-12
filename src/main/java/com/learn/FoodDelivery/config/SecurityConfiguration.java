5a2f605c8b6e854ffa0ea5404bd5a09064c52baa
AniketPsycho <aniket.patel9786@gmail.com>
2024-02-12 17:19:33 +0530

    cors enable all

--- src/main/java/com/learn/FoodDelivery/config/SecurityConfiguration.java
+++ src/main/java/com/learn/FoodDelivery/config/SecurityConfiguration.java
@@ -29,7 +29,7 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
 
         http
-                .csrf(AbstractHttpConfigurer::disable)
+                //.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests( auth -> auth
                                                 .requestMatchers(WHITELIST_URL_PATTERN)
                                                 .permitAll()
